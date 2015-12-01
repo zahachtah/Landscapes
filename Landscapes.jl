6f2615d47925a6b3e28a3abd7b7ce0c960fbc18f
zahachtah <jon.norberg@ecology.su.se>
2015-12-01 14:43:15 +0100

    edit

--- Landscapes.jl
+++ Landscapes.jl
@@ -22,7 +22,8 @@ type par
   reprod::Float64
   dispersalAlpha::Float64
   dispersalC::Float64
-  localTvar::Float64
+  LDDED::Float64
+  LDDVD::Float64
   overWinter::Float64
   seedPerBiomass::Float64
   extent::Float64
@@ -61,29 +62,23 @@ function simE(T::Float64,p::par,x::Array{Float64,1},tr::Array{Float64,1})
   return 0.0,x
 end
 
-function Go(L::Int64,T::Int64)
-  p=Landscapes.getP()
-  p.NoLandscape=L
-  p.inData="/Users/Raukhur/Documents/Github/Landscapes/inData"
-  p.outData="/Users/Raukhur/Documents/Github/Landscapes/outData"
-  p=Landscapes.GetData(p)
-  if L>4
-    p.XY=p.XY*1000
-    p.extent=p.extent*1000
-  end
-  p.seedPerBiomass=1/Landscapes.dispersal(10000.0,5000.0,0.5)
-  p.dispersalAlpha=5000.0
-  p.Tend=T
-  out=Landscapes.Go(p)
-  p.Poisson=2
-  out=Landscapes.Go(p)
-  p.Poisson=0
-  out=Landscapes.Go(p)
-end
+"
+Starts a full simulation based on parameter type definition. Generate a parameter type and redefine variables as needed like:
 
+   ```
+    p=getP()
+    p.Tend=2000,
+    ```
+
+then run simulation as
+
+   `Go(p)`
+
+"
 function Go(p::par)
   srand(1234+p.NoSpecies+p.NoLandscape*10+p.repl*100+p.NoNoise*1000+p.Tend*10000) # sets a random sequence that is different for all
   r=Float64
+  LDDf=Float64
   x=zeros(Float64,p.NoSpecies,1)+0.5/p.NoSpecies
   X=zeros(Float64,p.Tend,p.NoSpecies,p.NoSites)
   IE=zeros(Float64,p.Tend,p.NoSpecies,p.NoSites)
@@ -93,34 +88,41 @@ function Go(p::par)
   I=Array(Float64,p.NoSites)
   SD=Array(Float64,p.NoSites)
   timed=zeros(Float64,p.Tend)
-  progress=Progress(p.Tend,1)
   Tactual=zeros(Float64,p.Tend,p.NoSites)
   X[1,:,:]=0.5/p.NoSpecies
   T=0.0
+
   for t=1:p.Tend
-    next!(progress)
-    TC=CC(t,p) # Get climate change
-    SM,TotD,Dist,DS=addSouth(X,t-1,p)
+    # Get climate change
+    TC=CC(t,p)
+
+    # Get Distribution coming from south "mainland"
+    SM,TotD,Dist,DS=southDist(X,t-1,p)
     SDX[t,:]=DS'*TotD
-    if t==p.CCstart
-      XCS[1,:,:]=X[max(1,t-1),:,:];
+
+    # Remove traversing species
+    if t==p.CCstart && p.Poisson==2
+      XCS[1,:,:]=mean(X[p.CCstart-26:p.CCstart-1,:,:],1); ## Better average over last stable 25 years !!
       XCS[find(XCS.>0.0)]=1.0;
-      #("adjust XCS")
     end
+
     for j=1:p.NoSites
       if t>1
-        SD=DS'*TotD*p.reprod*dispersal(abs(p.XY[2,j]-p.sDist),p.dispersalAlpha,p.dispersalC)
-        if p.Poisson==2
-          I=immigration(X[t-1,:,:].*XCS[end,:,:],p,j)*p.reprod+SD
-        else
-          I=immigration(X[t-1,:,:],p,j)*p.reprod+SD
+
+        SD=addSouth(DS'*TotD,j,p)
+
+        I=immigration(X[t-1,:,:].*XCS[end,:,:],p,j)+SD
+
+        if p.LDDED>0.0
+          I=I+immigrationLDD(X[t-1,:,:].*XCS[end,:,:],p,j)+SD
         end
+
         for k=1:p.NoSpecies
           if p.Poisson>=1 || t<=p.CCstart
             if I[k]>0.0 #self immigration is zero
               # Maybe separate dispersal scaling parameter from seedBiomass
-              P=PoissonRnd(p::par,I[k])
-              r=X[t-1,k,j]*(1-p.reprod)+P*p.ext #p.ext=min propagule biomass
+              P=PoissonRnd(p::par,I[k]*p.seedPerBiomass)
+              r=P*p.ext #p.ext=min propagule biomass
             else
               r=0.0
               P=0.0
@@ -130,42 +132,43 @@ function Go(p::par)
           elseif p.Poisson==0 && t>p.CCstart
             r=0.0
           else
-            r=X[t-1,k,j]*(1-p.reprod)*p.overWinter+I[j]
+            r=X[t-1,k,j]*p.overWinter+I[j]
           end
 
-          x[k]=X[t-1,k,j]*(1-p.reprod)*p.overWinter+r
+          x[k]=X[t-1,k,j]*p.overWinter+r
           # remove call to dispersal out of loop
         end
       x[find(x.<p.ext)]=0.0
       end
       Tactual[t,j]=TC+p.noise[t]+p.tempGrad[j]
       tout,yout=sim(Tactual[t,j],p,x[:],[0.0;180.0])
-      X[t,:,j]=yout[end][:]
+      X[t,:,j]=yout
     end
   end
     file=p.outData*"/out"*string(p.NoLandscape)*"_"*string(p.Poisson)*".h5"
-    A=h5open(file,"w") do file
-       write(file,"X", X)
-       write(file,"Tactual", Tactual)
-       write(file,"XCS", XCS)
-       write(file,"ISD", ISD)
-       write(file,"IE", IE)
-       write(file,"SDX", SDX)
-      write(file,"sDist",p.sDist)
-  end
+    A=h5open(file,"w")
+       A["X","compress",3]=X
+       A["Tactual","compress",3]=Tactual
+       A["XCS","compress",3]=XCS
+       A["ISD","compress",3]=ISD
+       A["IE","compress",3]=IE
+       A["SDX","compress",3]=SDX
+       A["sDist","compress",3]=p.sDist
+    close(A)
   return X,XCS,IE,ISD
 end
 
-function Go(NoSpecies::Int64,Landscape::Int64,repl::Int64,NoiseSeries::Int64,alpha::Float64,Tend::Int64,PoissonRand::Int64,_Save::Int64)
-  p=getP(Landscape,repl,NoiseSeries,Tend,alpha)
-  p.Poisson=PoissonRand
-  X,XCS,IE,ISD,p,timed=Go(p,_save)
-  return X,XCS,IE,ISD,p,timed
+function sim(T::Float64,p::par,x::Array{Float64,1},tr::Array{Float64,1})
+  tout,tout=ode23((t,x)->dy(t,x,T,p),x,tr)
+  yout=tout[end][:]
+  return tout,yout
 end
 
-function sim(T::Float64,p::par,x::Array{Float64,1},tr::Array{Float64,1})
-  tout,yout=ode23((t,x)->dy(t,x,T,p),x,tr)
-  return tout,yout
+function simE(T::Float64,p::par,x::Array{Float64,1},tr::Array{Float64,1})
+  for t in tr
+    x=max(0,x+dy(t,x,T,p).*x)
+  end
+  return 1.0,x
 end
 
 function CC(t,p)
@@ -213,6 +216,14 @@ function dispersal(D::Float64,a::Float64,c::Float64)
   return d
 end
 
+function dispersal(D::Array{Float64},a::Float64,c::Float64)
+  d=zeros(Float64,length(D))
+  for i=1:length(D)
+  d[i]=dispersal(D[i],a,c)
+  end
+  return d
+end
+
 function immigration(X,p,site)
   I=zeros(Float64,1,p.NoSpecies)
   for i=1:p.NoSpecies
@@ -223,14 +234,29 @@ function immigration(X,p,site)
   return I
 end
 
-function addSouth(X,tt,p)
+function immigrationLDD(X,p,site)
+  LDDf=Float64
+  if p.LDDED>0.0
+    LDDf=1/dispersal(p.LDDED,p.LDDVD,p.dispersalC)/p.seedPerBiomass
+  end
+  I=zeros(Float64,1,p.NoSpecies)
+  for i=1:p.NoSpecies
+    for j=1:p.NoSites
+      I[i]=I[i]+LDDf*dispersal(abs(p.XY[2,j]-p.XY[2,site]),p.LDDVD,p.dispersalC)*X[end,i,j]
+    end
+  end
+  return I
+end
+
+function southDist(X,tt,p)
 # Make sure the out variable is the distribution put on a 1:NoSPecies array at the right place
 S=zeros(Float64,p.NoSites,1)
 Dist=zeros(Float64,p.NoSpecies+1,1)
 N=zeros(Float64,p.NoSpecies+1,1)
 DS=zeros(Float64,p.NoSpecies,1)
+SM=Float64
 T=0.0
-  t=max(tt,1)
+t=max(tt,1)
   for i=1:p.NoSites
     M=0.0
     #M=sum(X[t,:,i].*p.z')/sum(X[t,:,i])
@@ -238,42 +264,53 @@ T=0.0
     T=T+sum(X[t,:,i])
     S[i,:]=M
     id=indmax(X[t,:,i])
-    if id<p.NoSpecies/2
-      q=X[t,1:p.NoSpecies-((p.NoSpecies/2-id)*2+1),i]
-    elseif id>p.NoSpecies/2
-      q=X[t,2*(id-p.NoSpecies/2):p.NoSpecies,i]
+    if id<p.NoSpecies÷2
+      q=X[t,1:p.NoSpecies-((p.NoSpecies÷2-id)*2+1),i]
+    elseif id>p.NoSpecies÷2
+      q=X[t,2*(id-p.NoSpecies÷2):p.NoSpecies,i]
     else
       q=zeros(Float64,1,p.NoSpecies+1)
       q[2:p.NoSpecies+1]=X[t,:,i]
     end
-    dd=(p.NoSpecies-(length(q)-1))/2
+    dd=(p.NoSpecies-(length(q)-1))÷2
     Dist[1+dd:p.NoSpecies+1-dd]=Dist[1+dd:p.NoSpecies+1-dd]+q'./sum(q)
     N[1+dd:p.NoSpecies+1-dd]=N[1+dd:p.NoSpecies+1-dd]+1
   end
-  T=T/p.NoSites
-  reg=[p.XY[2,:],ones(Float64,p.NoSites,1)']'\S
-  SM=reg[2]+p.sDist*reg[1] #CHECK THAT THIS IS RIGHT
   Dist=Dist./N
-  idm=indmin((p.z.-SM).^2)
-  if p.z[idm]>SM
-    idm=idm-1
-  end
-  for i=1:p.NoSpecies
-    if idm-50+i>0 && idm-50+i<=p.NoSpecies
-      DS[idm-50+i]=Dist[i]*(SM-p.z[idm])+Dist[i+1]*(1-(SM-p.z[idm]))
+  Dist[isnan(Dist)]=0.0 #some values that where zero get Nan, check
+  T=T/p.NoSites
+  reg=[p.XY[2,:];ones(Float64,p.NoSites,1)']'\S
+
+  Xdist=collect(linspace(0.0,100000.0,round(p.NoSpecies/p.extent*100000)))
+  for j=1:length(Xdist)
+    dp=dispersal(abs(p.XY[2,1]+p.sDist-Xdist[j]),p.dispersalAlpha,p.dispersalC)
+    if p.LDDED>0.0
+      LDDf=1/dispersal(p.LDDED,p.LDDVD,p.dispersalC)/p.seedPerBiomass
+      dp=dp+LDDf*dispersal(abs(p.XY[2,1]+p.sDist-Xdist[j]),p.LDDVD,p.dispersalC)
+    end
+    SM=reg[2]+(p.sDist-Xdist[j])*reg[1]
+
+    idm=indmin((p.z.-SM).^2)
+    if p.z[max(1,idm)]>SM
+      idm=max(1,idm-1)
+    end
+    for i=1:p.NoSpecies
+      if idm-50+i>0 && idm-50+i<=p.NoSpecies
+        DS[idm-50+i]=DS[idm-50+i]+Dist[i]*(SM-p.z[idm])+Dist[i+1]*(1-(SM-p.z[idm]))
+      end
     end
   end
-  #for i=1:idm
-  #  if p.NoSpecies/2+1-(idm-i)>=1
-  #    DS[i]=Dist[p.NoSpecies/2+1-(idm-i)]
-  #  end
-  #end
-  #for i=idm+1:p.NoSpecies
-  #  if i-idm<p.NoSpecies/2
-  #    DS[i]=Dist[p.NoSpecies/2+i-idm+1]
-  #  end
-  #end
+  DS=DS/sum(DS)
   return SM,T,Dist,DS
+end
+
+function addSouth(D,j,p)
+  return D.*dispersal(abs(p.XY[2,j]+p.sDist),p.dispersalAlpha,p.dispersalC)
+end
+
+function addSouthLDD(D,j,p)
+  LDDf=1/dispersal(p.LDDED,p.LDDVD,p.dispersalC)/p.seedPerBiomass
+  return D.*LDDf*dispersal(abs(p.XY[2,j]+p.sDist),p.LDDVD,p.dispersalC)
 end
 
 end
